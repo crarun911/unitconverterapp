64048963d5e87c23e822d90e7f5cd4a1293c6a1c
Arun Rajendran <crarun7453@gmail.com>
2025-03-07 12:30:01 +0000

    MainActivity file modiied button composables are arranged  center both vertically and horizontally

--- app/src/main/java/com/dcoders/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/dcoders/myfirstapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.dcoders.myfirstapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -17,6 +18,7 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.dcoders.myfirstapp.ui.theme.MyFirstAppTheme
@@ -41,7 +43,10 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UnitConverterUi() {
-    Column {
+    Column (modifier = Modifier.fillMaxSize(),
+            verticalArrangement =Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+            )       {
         Text(text = "Unit Converter")
         OutlinedTextField(value = "", onValueChange = {})
         Row {
